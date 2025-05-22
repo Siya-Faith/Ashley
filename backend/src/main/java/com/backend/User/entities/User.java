850cc773c232764f250f1539467b9ff0fe3bdd5a
SIPHAMANDLA NGCEPE <sphashepherd@gmail.com>
2025-05-23 01:59:14 +0200

    database

---
+++ backend/src/main/java/com/backend/User/entities/User.java
@@ -0,0 +1,43 @@
+package com.backend.User.entities;
+
+import com.backend.Features.Ward.entities.Ward;
+import com.backend.User.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ward_id")
+    private Ward ward;
+
+    @Column(name = "created_time", insertable = false, updatable = false)
+    private LocalDateTime createdTime;
+
+
+}
